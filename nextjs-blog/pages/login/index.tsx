014860b253d0a2469f4d487f8e7c0cb53b564dbe
a.marchenko <amarchenko.exceedteam@gmail.com>
2022-08-10 14:54:43 +0300

    add regestration page and add valitation for log and reg component

--- nextjs-blog/pages/login/index.tsx
+++ nextjs-blog/pages/login/index.tsx
@@ -1,16 +1,94 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './Login.module.scss'
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Router from 'next/router'
+import * as Yup from 'yup';
+import axios from "axios";
 
 const LoginPage = () => {
-  return <div className = {styles.containetLogIn}> 
-      <div className= {styles.titleLog}>
+
+  const [authOpen, setAuthOpen] = useState(false)
+
+  const validationSchema = Yup.object().shape({ 
+    email: Yup.string()
+        .required('Email is required')
+        .email('Email is invalid'),
+    password: Yup.string()
+        .min(6, 'Password must be at least 6 characters')
+        .required('Password is required'),
+  });
+const formOptions = { resolver: yupResolver(validationSchema) };
+
+  // get functions to build form with useForm() hook
+  const { register, handleSubmit, reset, formState } = useForm(formOptions);
+  const { errors } = formState;
+
+
+  const authLogin = async(data) => {
+
+    const {email, password} = data
+
+    try{
+      await axios
+        .post("http://localhost:8000/auth/login", {
+          email,
+          password,
+        })
+        .then((res) => {
+          localStorage.setItem("token", res.data);
+          Router.push('/')
+        });
+    } catch {
+      console.log('---------->penis');
+    }
+  }
+  
+  if (authOpen) return (
+    <div className = {styles.containetLogIn}> 
+    <div className={styles.titleLog}>
+      Log in
+    </div>
+    <form onSubmit={handleSubmit(authLogin)}>
+      <div className={styles.formConatiner}>
+        E-mail
+        <div className="invalid-feedback">{errors.email?.message}</div>
+        <input type='text'
+              {...register('email')}                                                                                       
+              className={styles.formLog}
+              placeholder='E-mail'
+        />
+        Password
+        <div className="invalid-feedback">{errors.password?.message}</div>
+        <input type='password'
+            {...register('password')}
+              className={styles.formLog}
+              placeholder='Password'
+        />
+      
+      </div>
+      <div className={styles.btnContainer}>
+        <button className={styles.btnSing} 
+              onClick={authLogin}
+        >
+          Sign Up
+        </button>
+      </div>
+    </form>
+  </div>  
+  )
+
+  return (
+    <div className = {styles.containetLogIn}> 
+      <div className={styles.titleLog}>
         Log in
       </div>
-      <div className= {styles.btnGroup}>
-        <button className= {styles.btnLog}>Log in with Auth0</button>
-        <button className= {styles.btnLog}>Log in with e-mail</button>
+      <div className={styles.btnGroup}>
+        <button className={styles.btnLog}>Log in with Auth0</button>
+        <button className={styles.btnLog} onClick={() => setAuthOpen(!authOpen)}>Log in with e-mail</button>
       </div>
-   </div>;
+    </div>  
+  )
 };
 
 export default LoginPage;
--- nextjs-blog/pages/registration/index.tsx
+++ nextjs-blog/pages/registration/index.tsx
@@ -1,35 +1,102 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './Registration.module.scss'
+import axios from "axios";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Router from 'next/router'
+import * as Yup from 'yup';
 
-function RegistrationPage() {
+const RegistrationPage = () => {
+
+  const validationSchema = Yup.object().shape({ 
+    username: Yup.string()
+        .required('First Name is required'),
+    email: Yup.string()
+        .required('Email is required')
+        .email('Email is invalid'),
+    password: Yup.string()
+        .min(6, 'Password must be at least 6 characters')
+        .required('Password is required'),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Passwords must match')
+        .required('Confirm Password is required'),
+    // acceptTerms: Yup.bool()
+    //     .oneOf([true], 'Accept Ts & Cs is required')
+  });
+const formOptions = { resolver: yupResolver(validationSchema) };
+
+ // get functions to build form with useForm() hook
+ const { register, handleSubmit, reset, formState } = useForm(formOptions);
+ const { errors } = formState;
+
+  const sendCreateNewUser = async(data) => {
+
+    const {username, email, password} = data
+      try{
+        await axios
+        .post("http://localhost:8000/auth/registration", {
+          email,
+          username,
+          password,
+        })
+        .then((res) => {
+          localStorage.setItem("token", res.data.token);
+          Router.push('/')
+        });
+      } catch{
+        console.log('---------->hyevoz');
+      }
+  }
+
   return (
-    <div className={styles.containerReg}>
-      <div className={styles.titleReg}>
-        Sign up
-      </div>
-      <div className={styles.btnGroup}>
-        Username
-        <input type='text'
-               className={styles.btnReg}
-               placeholder='Username'
-        />
-        E-mail
-        <input type='text'
-               className={styles.btnReg}
-               placeholder='E-mail'
-        />
-        Password
-        <input type='password'
-               className={styles.btnReg}
-               placeholder='Password'
-        />
-        Re-enter password
-        <input type='password'
-               className={styles.btnReg}
-               placeholder='Re password'
-        />
-      </div>
-    </div>
+      <form onSubmit={handleSubmit(sendCreateNewUser)}>
+        <div className={styles.containerReg}>
+          <div className={styles.titleReg}>
+            Sign up
+          </div>
+          <div className={styles.btnGroup}>
+            Username
+            <div className="invalid-feedback">{errors.username?.message}</div>
+            <input type='text'
+                  {...register('username')}
+                  className={styles.btnReg}
+                  placeholder='Username'
+            />
+            E-mail
+            <div className="invalid-feedback">{errors.email?.message}</div>
+            <input type='text'
+                  {...register('email')}                                                                                       
+                  className={styles.btnReg}
+                  placeholder='E-mail'
+            />
+            Password
+            <div className="invalid-feedback">{errors.password?.message}</div>
+            <input type='password'
+                 {...register('password')}
+                  className={styles.btnReg}
+                  placeholder='Password'
+            />
+            Re-enter password
+            <div className="invalid-feedback">{errors.confirmPassword?.message}</div>
+            <input type='password'
+                  {...register('confirmPassword')}
+                  className={styles.btnReg}
+                  placeholder='Re password'
+            />
+          </div>
+          <div className={styles.terms}>
+          If you click the Sign up button, you are considered 
+          to agree to the Terms of Use and Privacy Policy.
+          </div>
+          <div className={styles.btnContainer}>
+            <button className={styles.btnSing} 
+                    onClick={sendCreateNewUser}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </form>
   )
 }
 
